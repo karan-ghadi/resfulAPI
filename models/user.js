c0fa178f992be23387e769f9ac44a709123ec9a2
Karan Ghadi <karan@esscale.co>
2018-10-05 12:00:20 +0530

    updated code

--- models/user.js
+++ models/user.js
@@ -4,7 +4,12 @@ const userSchema = mongoose.Schema({
 	_id: mongoose.Schema.Types.ObjectId,
 	firstName: String,
 	lastName: String,
-	userName: String
+	userName: {
+		type: String,
+		required: true,
+		unique: true,
+		lowercase: true
+	}
 });
 
 module.exports = mongoose.model('User', userSchema);
--- routes/users.js
+++ routes/users.js
@@ -4,9 +4,42 @@ const User = require('../models/user');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-	res.status(200).json({
-		message: 'user get request'
-	});
+	User.find()
+		.exec()
+		.then(d => {
+			res.status(201).json({
+				users: d
+			});
+		})
+		.catch(e => {
+			res.status(404).json({
+				message: 'no records available'
+			});
+		});
+});
+
+router.get('/:userId', (req, res, next) => {
+	const id = req.params.userId;
+	User.findOne({
+			_id: id
+		})
+		.exec()
+		.then(d => {
+			if (d === null) {
+				res.status(201).json({
+					message: 'no user available'
+				});
+			} else {
+				res.status(201).json({
+					users: d
+				});
+			}
+		})
+		.catch(e => {
+			res.status(404).json({
+				message: 'something went wrong'
+			});
+		});
 });
 
 router.post('/', (req, res, next) => {
@@ -16,25 +49,72 @@ router.post('/', (req, res, next) => {
 		firstName: data.firstName,
 		lastName: data.lastName,
 		userName: data.userName
-	})
-
-	user.save().then((result => {
-		res.status(201).json({
-			message: 'User created successfully!..',
-			user: user
-		});
-	})).catch((err) => {
-		res.status(404).json({
-			message: err
-		});
-	})
-
+	});
+	User.findOne({
+			userName: data.userName
+		})
+		.exec()
+		.then(d => {
+			if (d) {
+				res.status(400).json({
+					message: 'username already used'
+				});
+			} else {
+				user
+					.save()
+					.then(result => {
+						res.status(201).json({
+							message: 'User created successfully!..',
+							user: user
+						});
+					})
+					.catch(err => {
+						res.status(404).json({
+							message: err
+						});
+					});
+			}
+		})
+		.catch(e => {
+			res.status(404).json({
+				message: err
+			});
+		})
 });
 
 router.delete('/:userId', (req, res, next) => {
-	res.status(200).json({
-		message: 'user deleted'
-	});
+	const id = req.params.userId;
+	User.findById(id)
+		.exec()
+		.then(result => {
+			if (result === null) {
+				res.status(404).json({
+					message: 'User not Found'
+				});
+			} else {
+				User.deleteOne({
+						_id: id
+					})
+					.exec()
+					.then(result => {
+						console.log(result);
+						res.status(200).json({
+							message: 'user deleted with id ' + id
+						});
+					})
+					.catch(err => {
+						console.log(err);
+						res.status(200).json({
+							message: err
+						});
+					});
+			}
+		})
+		.catch(err => {
+			res.status(404).json({
+				message: err
+			});
+		});
 });
 
 module.exports = router;
